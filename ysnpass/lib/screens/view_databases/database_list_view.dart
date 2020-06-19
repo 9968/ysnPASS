608a4da5b85d04271e0a2a3bb3b054dd9afb537e
9968 <99six8@protonmail.com>
2020-06-19 14:19:24 +0200

    open database correctly from database list view

--- ysnpass/lib/screens/view_databases/database_list_view.dart
+++ ysnpass/lib/screens/view_databases/database_list_view.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:ysnpass/screens/view_database/index.dart';
+import 'package:ysnpass/store/actions/actions.dart';
 import 'package:ysnpass/store/models/app_state.dart';
 import 'package:ysnpass/store/models/database.dart';
 import 'package:ysnpass/store/selectors/selectors.dart';
 
+class DatabaseListViewModel {
+  final List<Database> databaseList;
+  final Function openDatabase;
+
+  DatabaseListViewModel(this.databaseList, this.openDatabase);
+}
+
 class DatabaseListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, List<Database>>(
-        converter: (store) => databasesSelector(store.state),
-        builder: (context, databaseList) {
+    return StoreConnector<AppState, DatabaseListViewModel>(
+        converter: (store) => DatabaseListViewModel(
+              databasesSelector(store.state),
+              (databaseId) => store.dispatch(
+                OpenDatabaseAction(databaseId),
+              ),
+            ),
+        builder: (context, databaseListViewModel) {
           return ListView(
-              children: databaseList
+              children: databaseListViewModel.databaseList
                   .map((database) => ListTile(
                         title: Text(database.name),
                         trailing: Icon(Icons.chevron_right),
                         onTap: () {
+                          databaseListViewModel.openDatabase(database.id);
                           Navigator.push(
                             context,
                             MaterialPageRoute(
