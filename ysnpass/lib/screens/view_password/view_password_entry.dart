6f568ce6add2dfe6067e2eef942fe77ff712eede
9968 <99six8@protonmail.com>
2020-06-28 13:32:49 +0200

    make show password screen prettier

--- ysnpass/lib/screens/view_password/view_password_entry.dart
+++ ysnpass/lib/screens/view_password/view_password_entry.dart
@@ -4,10 +4,17 @@ import 'package:ysnpass/screens/view_password/view_password_entry_model.dart';
 
 import 'delete_password_entry_button.dart';
 
-class ViewPasswordEntry extends StatelessWidget {
+class ViewPasswordEntry extends StatefulWidget {
   final ViewPasswordEntryModel viewPasswordEntryModel;
 
   ViewPasswordEntry(this.viewPasswordEntryModel);
+
+  @override
+  _ViewPasswordEntryState createState() => _ViewPasswordEntryState();
+}
+
+class _ViewPasswordEntryState extends State<ViewPasswordEntry> {
+  bool showPassword = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +22,28 @@ class ViewPasswordEntry extends StatelessWidget {
       appBar: AppBar(
         title: Text('Password'),
       ),
-      body: Column(
-        children: [
-          Text(viewPasswordEntryModel.passwordEntry.username),
-          Text(viewPasswordEntryModel.passwordEntry.password),
-          DeletePasswordEntryButton(
-              deleteCallback: viewPasswordEntryModel.deletePassword),
-        ],
-      ),
+      body: Column(children: [
+        ListView(
+          shrinkWrap: true,
+          children: [
+            ListTile(
+              title: Text(widget.viewPasswordEntryModel.passwordEntry.username),
+            ),
+            ListTile(
+              title: Text(
+                showPassword
+                    ? widget.viewPasswordEntryModel.passwordEntry.password
+                    : String.fromCharCode(0x25CF) * 10,
+              ),
+              trailing: IconButton(
+                  icon: Icon(Icons.remove_red_eye),
+                  onPressed: _toggleShowPassword),
+            )
+          ],
+        ),
+        DeletePasswordEntryButton(
+            deleteCallback: widget.viewPasswordEntryModel.deletePassword),
+      ]),
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.edit,
@@ -34,11 +55,15 @@ class ViewPasswordEntry extends StatelessWidget {
             context,
             MaterialPageRoute(
               builder: (context) => EditPasswordScreen(
-                  passwordEntry: viewPasswordEntryModel.passwordEntry),
+                  passwordEntry: widget.viewPasswordEntryModel.passwordEntry),
             ),
           );
         },
       ),
     );
   }
+
+  _toggleShowPassword() => setState(() {
+        showPassword = !showPassword;
+      });
 }
