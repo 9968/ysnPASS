fbc1cfe22d4b7dcec4c7a8d146abdfebe4e313a5
yaene <yaene@9968.ch>
2021-01-29 22:17:24 +0100

    remove desktop app from mobile repo

--- ysnpass-desktop/src/app/store/reducers/app-state.reducers.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordEntry, passwordListReducer } from './password-list.reducer';
-
-export interface AppState {
-  passwordList: PasswordEntry[];
-}
-export const appReducers = {
-  passwordList: passwordListReducer,
-};
--- ysnpass-desktop/src/app/store/selectors/password-list.selectors.ts
+++
@@ -1,5 +0,0 @@
-import { AppState } from '../reducers/app-state.reducers';
-
-export const selectPasswordList = (state: AppState) => {
-  return state.passwordList;
-};
